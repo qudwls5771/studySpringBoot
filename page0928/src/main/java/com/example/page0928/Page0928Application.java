49f1fc4234a40c1c9f47638752e5005786147bc8
qudwls5771 <qudwls5771@naver.com>
2022-09-28 09:26:57 +0900

    페이지

---
+++ page0928/src/main/java/com/example/page0928/Page0928Application.java
@@ -0,0 +1,13 @@
+package com.example.page0928;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Page0928Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Page0928Application.class, args);
+    }
+
+}
---
+++ page0928/src/test/java/com/example/page0928/Page0928ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.page0928;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Page0928ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
